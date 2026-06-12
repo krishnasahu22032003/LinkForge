db0a033b4eb56e3b935b20a2fb73e66fea8a04f1
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-13 01:03:26 +0530

    half signup done

--- backend/src/controller.ts/UserController.ts
+++ backend/src/controller.ts/UserController.ts
@@ -1,7 +1,65 @@
 import type { Request, Response } from "express";
+import { SignUpSchema } from "../validatons/auth.schema.js";
+import bcrypt from "bcrypt";
+import { prisma } from "../lib/prisma.js";
+import crypto from "crypto" ;
 
-export function UserSignUp(req : Request , res:Response){
+const SALT_ROUNDS = 12 ;
 
+export async function UserSignUp(req : Request , res:Response){
 
+const parsedData = SignUpSchema.safeParse(req.body);
+
+if(!parsedData.success){
+    return res.status(400).json({
+        success:false ,
+        message:"Invalid Input",
+        error:parsedData.error.flatten()
+    });
+};
+
+const {username , email , password} = parsedData.data ; 
+
+try{
     
+    const checkuser = await prisma.user.findUnique({
+        where:{
+            email
+        }
+    }) ;
+
+    if(checkuser){
+        
+        return res.status(400).json({
+            success:false ,
+            message:"User with this email already exists"
+        });
+    };
+
+    const hashedPassword  = await bcrypt.hash(password , SALT_ROUNDS) ;
+
+    const user = await prisma.user.create({
+        data:{
+            email,
+            password:hashedPassword , 
+            username,
+            emailVerified:false
+        }
+    });
+
+    const token = crypto.randomUUID ; 
+
+   await prisma.verificationToken.create({
+    data:{
+        token ,
+        userId:user.id,
+        expiresAt:new Date(Date.now() + 1000 * 60 * 60 * 24),
+
+    },
+   });
+
+}catch(error){
+
+}
+
 }
