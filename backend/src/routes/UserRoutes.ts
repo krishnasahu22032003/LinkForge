0d31da3282640bd675f9066281547cfe08a0d3e7
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-13 03:07:45 +0530

    user routes code update

--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -1,9 +1,14 @@
-import express from "express" ; 
-import { UserSignUp, verifyEmail } from "../controller.ts/UserController.js";
+import express from "express";
+import { GetUserDetail, updateUserDetails, UserSignIn, UserSignOut, UserSignUp, verifyEmail } from "../controller.ts/UserController.js";
+import authMiddleware from "../middlewares/authmiddleware.js";
 
-const UserRouter = express.Router() ;
+const UserRouter = express.Router();
 
-UserRouter.post("/signup" , UserSignUp) ;
-UserRouter.post("/verifyEmail" , verifyEmail) ;
+UserRouter.post("/signup", UserSignUp);
+UserRouter.post("/verifyEmail", verifyEmail);
+UserRouter.post("/signin", UserSignIn);
+UserRouter.get("/me", authMiddleware, GetUserDetail);
+UserRouter.post("/Signout", authMiddleware, UserSignOut);
+UserRouter.patch("/update", authMiddleware, updateUserDetails);
 
-export default UserRouter ;
+export default UserRouter;
