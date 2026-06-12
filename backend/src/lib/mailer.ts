74c3eac791a23e149df037b13c9d4701817400be
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-13 01:12:10 +0530

    email service code done

--- backend/src/lib/mailer.ts
+++ backend/src/lib/mailer.ts
@@ -1,9 +1,11 @@
 import nodemailer from "nodemailer" ;
 
-const transporter = nodemailer.createTransport({
+ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
 });
+
+export default transporter ;
---
+++ backend/src/services/email.service.ts
@@ -0,0 +1,23 @@
+import transporter from "../lib/mailer.js";
+
+export async function sendVerificationEmail(
+  email: string,
+  token: string
+) {
+  const verificationUrl =
+    `${process.env.FRONTEND_URL}/verify-email?token=${token}`;
+
+  await transporter.sendMail({
+    to: email,
+    subject: "Verify your email",
+    html: `
+      <h1>Welcome to LinkForge</h1>
+
+      <p>Click below to verify your email:</p>
+
+      <a href="${verificationUrl}">
+        Verify Email
+      </a>
+    `,
+  });
+}
