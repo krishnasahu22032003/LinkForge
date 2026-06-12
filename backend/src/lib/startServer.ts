fc96478ead0057bea99b21b889906c42f2df83c1
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-13 00:21:53 +0530

    start server function done

--- backend/src/lib/startServer.ts
+++ backend/src/lib/startServer.ts
@@ -1,13 +1,18 @@
 import type { Express } from "express";
 import ENV_SECRETS from "./ENV.js";
 
-export default function startServer(app:Express){
+export default function startServer(app: Express) {
 
-try{
+    const http_port = ENV_SECRETS.PORT;
 
-app.listen()
+    try {
 
-}
+        app.listen(http_port, () => { console.log(`App running on ${http_port}`) });
 
+    } catch (err) {
+
+        console.error((err as Error).message, "Internal server error");
+        process.exit(1);
+    };
 
 }
