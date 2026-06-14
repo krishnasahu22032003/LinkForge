6507f9b662ae564f506cde0f7bffa0c5d1c4310d
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-15 01:28:28 +0530

    generateshortcode function add

--- backend/src/controller.ts/UrlController.ts
+++ backend/src/controller.ts/UrlController.ts
@@ -1,7 +1,19 @@
 import type { Request , Response } from "express";
+import { createShortUrlSchema } from "../validatons/url.schema.js";
+import { parse } from "node:path";
 
 
 export async function CreateUrl(req: Request , res:Response){
+
+const parsedData = createShortUrlSchema.safeParse(req.body) ;
+
+if(!parsedData.success){
+    return res.status(400).json({
+        success:false,
+        message:"Invalid Input",
+        error:parsedData.error.flatten()
+    });
+};
 
 
 
