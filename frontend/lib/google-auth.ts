b1a691f6923e09f27254c78491ab0b69ed572877
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-16 03:58:54 +0530

    google singin function added

---
+++ frontend/lib/google-auth.ts
@@ -0,0 +1,27 @@
+import AxiosInstance from "./axios";
+
+interface Response {
+
+    success:boolean,
+    message:string ,
+    user:{
+        id : string ,
+        email:string ,
+        username:string
+    }
+};
+
+export default async function GoogleAuth (credential:string):Promise<Response>{
+
+    try{
+
+        const res = await AxiosInstance.post<Response>("/api/v1/user/google" , credential) ;
+        return res.data ;
+
+    }catch (error: any) {
+
+        throw new Error(error.response?.data?.message || "Registration failed");
+
+    };
+
+};
