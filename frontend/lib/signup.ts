9806a3c15e9ac125b2098d8c79cb6690cfca95eb
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-16 01:21:56 +0530

    signup function add

--- frontend/lib/signup.ts
+++ frontend/lib/signup.ts
@@ -1,3 +1,36 @@
-import ENV_SECRETS from "./ENV";
-import axios from "axios";
+import AxiosInstance from "./axios";
+
+interface UserDetails {
+
+    username: string,
+    email: string,
+    password: string
+
+};
+
+interface SignUpResponse {
+
+    success: boolean,
+    message: string,
+    data: {
+        name: string,
+        email: string
+    }
+
+};
+
+export async function SignUp(UserData: UserDetails): Promise<SignUpResponse> {
+
+    try {
+
+        const res = await AxiosInstance.post<SignUpResponse>("/api/v1/user/signup", UserData);
+        return res.data;
+
+    } catch (error: any) {
+
+        throw new Error(error.response?.data?.message || "Registration failed");
+        
+    };
+
+};
 
