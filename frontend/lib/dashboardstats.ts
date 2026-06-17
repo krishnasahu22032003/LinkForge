2f2e73afa7de4cbfd024a96d13cd326963bd3d7c
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-17 19:38:16 +0530

    dashboard stats function add

---
+++ frontend/lib/dashboardstats.ts
@@ -0,0 +1,24 @@
+import AxiosInstance from "./axios";
+
+interface Response {
+
+    success:false ,
+    data:{
+         totalUrls:number ,
+        totalClicks:number
+    }
+};
+
+export default async function DashboardStats():Promise<Response>{
+
+    try{
+
+        const res = await AxiosInstance.get("/api/v1/url/dashboard/stats");
+        return res.data ;
+
+    }catch (error: any) {
+
+        throw new Error(error.response?.data?.message || "Registration failed");
+
+    };
+};
