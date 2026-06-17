91ec4c6b6abfa25ff6a230fa3a758795128bb955
krishna sahu <krishna.sahu.work@gmail.com>
2026-06-17 23:17:54 +0530

    url list component add

---
+++ frontend/components/ui/UrlList.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { AnimatePresence } from "framer-motion";
+import UrlCard from "./UrlCard";
+
+interface Props {
+  urls: Url[];
+  onDelete: (id: string) => void;
+  onAnalytics: (id: string) => void;
+}
+
+export default function UrlList({
+  urls,
+  onDelete,
+  onAnalytics,
+}: Props) {
+  return (
+    <div className="grid gap-5">
+      <AnimatePresence>
+        {urls.map((url) => (
+          <UrlCard
+            key={url.id}
+            {...url}
+            onDelete={onDelete}
+            onAnalytics={onAnalytics}
+          />
+        ))}
+      </AnimatePresence>
+    </div>
+  );
+}
