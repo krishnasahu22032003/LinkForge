cf6622ba1a49f8e47041a653895019339750b747
krishna sahu <krishna.sahu.work@gmail.com>
2026-07-20 01:46:30 +0530

    docker file add

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ const app = express() ;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:"http://localhost:3000" , credentials:true}));
+app.use(cors({origin:"https://linkforge.krishnastack.com" , credentials:true}));
 app.use("/api/v1/user", UserRouter);
 app.use("/api/v1/url", UrlRouter);
 
