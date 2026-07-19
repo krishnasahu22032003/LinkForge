c7b19dbc1d933c5e4ef6ac391ae2147a32de8fb1
krishna sahu <krishna.sahu.work@gmail.com>
2026-07-20 05:29:35 +0530

    update code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,6 +13,6 @@ app.use(cookieParser());
 app.use(cors({origin:"https://linkforge.krishnastack.com" , credentials:true}));
 app.use("/api/v1/user", UserRouter);
 app.use("/api/v1/url", UrlRouter);
-app.use("/ap1/v1/health" , HealthRouter)
+app.use("/api/v1/health" , HealthRouter)
 
 startServer(app) ;
